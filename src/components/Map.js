262655bd2489feecf9d63aea8b89beb9c3e6e2ee
benl95 <beno_1995@hotmail.com>
2021-01-12 21:13:05 +0100

    feat: switch between map views

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,38 +7,56 @@ class Map extends React.Component {
 		super(props);
 		this.renderMap = this.renderMap.bind(this);
 		this.myRef = React.createRef();
+		this.state = { currentData: 'main' };
 	}
 
 	componentDidMount() {
 		this.renderMap();
 	}
 
+	componentDidUpdate() {
+		this.renderMap();
+	}
+
 	renderMap() {
 		const svg = select(this.myRef.current);
-		const data = this.props.data;
+		const data =
+			this.state.currentData === 'main'
+				? this.props.MainData
+				: this.props.MinorData;
 		const width = this.props.width;
 		const height = this.props.height;
 
 		const projection = geoMercator().fitSize([width, height], data);
 		const pathGenerator = geoPath().projection(projection);
 
-		svg.selectAll('.province')
+		svg.selectAll('.municipality')
 			.data(data.features)
 			.join('path')
-			.attr('class', 'province')
+			.attr('class', 'municipality')
 			.attr('fill', '#C6F4C6')
 			.attr('d', (feature) => pathGenerator(feature))
 			.append('title')
-			.text((d) => d.properties.statnaam);
+			.text((d) => d.properties.municipality);
 	}
 
 	render() {
 		return (
-			<svg
-				ref={this.myRef}
-				width={this.props.width}
-				height={this.props.height}
-			></svg>
+			<div className="container">
+				<div>
+					<button onClick={() => this.setState({ currentData: 'main' })}>
+						{this.props.FirstButton}
+					</button>
+					<button onClick={() => this.setState({ currentData: 'minor' })}>
+						{this.props.SecondButton}
+					</button>
+				</div>
+				<svg
+					ref={this.myRef}
+					width={this.props.width}
+					height={this.props.height}
+				/>
+			</div>
 		);
 	}
 }
