2de464fd4a781beb548cb71e9e808ef064e83a9f
benl95 <beno_1995@hotmail.com>
2021-01-13 14:40:44 +0100

    Import naviation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './app.css';
 import municipalityData from '../data/municipalities.geo.json';
 import provinceData from '../data/provinces.geo.json';
 import Map from './Map';
+import Navigation from './Navigation';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Navigation />
 				<Map
 					width={900}
 					height={600}
