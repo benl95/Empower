4af8ea0b125c03fd3bb5053ba777e2c5f2a09a8c
benl95 <beno_1995@hotmail.com>
2021-01-14 19:50:22 +0100

    Create ZeroState component

---
+++ src/components/ZeroState.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './zerostate.css';
+
+class ZeroState extends React.Component {
+	render() {
+		return (
+			<div className="textContainer">
+				<h1>Welcome to the page of Empower 2.0</h1>
+				<p>
+					To analyse the sustainable energy production of click on the
+					province to display the data
+				</p>
+				<p>
+					To get specified data about the municipalities click on a
+					specific municipality
+				</p>
+				<h2>
+					Get inspired to become more <strong>green</strong>
+				</h2>
+			</div>
+		);
+	}
+}
+
+export default ZeroState;
