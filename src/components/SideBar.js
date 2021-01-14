1085f9e7f6fcdbbb65ed0319be1acf5ae841ab9a
benl95 <beno_1995@hotmail.com>
2021-01-14 19:50:11 +0100

    Nest ZeroState component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ZeroState from './ZeroState';
 import './sidebar.css';
 
 class SideBar extends React.Component {
 	render() {
 		return (
 			<div className="sidebarContainer">
-				<div className="contentContainer"></div>
+				<div className="contentContainer">
+					<ZeroState />
+				</div>
 			</div>
 		);
 	}
