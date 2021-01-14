2ea0f6c411d80a0ccd9a5cf24ffffcab95631700
benl95 <beno_1995@hotmail.com>
2021-01-14 19:49:55 +0100

    Remove state from component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,36 +2,15 @@ import React from 'react';
 import './navigation.css';
 
 class Navigation extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { active: false };
-	}
-
 	render() {
 		return (
 			<div className="navigationContainer">
 				<ul className="menuItemsContainer">
 					<li>
-						<button
-							className={this.state.active ? 'active' : 'off'}
-							onClick={() =>
-								this.setState({ active: !this.state.active })
-							}
-							href="#"
-						>
-							Home
-						</button>
+						<button>Home</button>
 					</li>
 					<li>
-						<button
-							className={this.state.active ? 'active' : 'off'}
-							onClick={() =>
-								this.setState({ active: !this.state.active })
-							}
-							href="#"
-						>
-							Tips
-						</button>
+						<button>Tips</button>
 					</li>
 				</ul>
 			</div>
