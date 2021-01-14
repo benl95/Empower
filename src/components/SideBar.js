a941a568b6b4a690c5eef036fe531ae5b41d71ed
benl95 <beno_1995@hotmail.com>
2021-01-14 17:42:56 +0100

    Create SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sidebar.css';
+
+class SideBar extends React.Component {
+	render() {
+		return (
+			<div className="sidebarContainer">
+				<div className="contentContainer"></div>
+			</div>
+		);
+	}
+}
+
+export default SideBar;
