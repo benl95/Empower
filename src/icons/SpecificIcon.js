e701c9d6f9d1f547bf46b3489ecb58805359e296
benl95 <beno_1995@hotmail.com>
2021-01-19 21:01:32 +0100

    Create svg components

---
+++ src/icons/SpecificIcon.js
@@ -0,0 +1,167 @@
+import * as React from "react";
+
+function SvgSpecificIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={136.982}
+      height={134.438}
+      {...props}
+    >
+      <g
+        data-name="Group 44"
+        transform="translate(-347.227 -275.02)"
+        fill="none"
+        stroke="#f89976"
+        strokeWidth={3}
+      >
+        <rect
+          data-name="Rectangle 64"
+          width={78.671}
+          height={78.671}
+          rx={8.215}
+          transform="translate(376.849 304.231)"
+          strokeMiterlimit={10}
+        />
+        <rect
+          data-name="Rectangle 65"
+          width={25.626}
+          height={25.626}
+          rx={2.676}
+          transform="translate(383.256 348.983)"
+          strokeMiterlimit={10}
+        />
+        <path data-name="Path 1071" d="M376.849 311.918h78.671" />
+        <path
+          data-name="Line 67"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 326.013h37.926"
+        />
+        <path
+          data-name="Line 68"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 331.558h25.602"
+        />
+        <path
+          data-name="Path 1072"
+          d="M414.007 337.562h31.231"
+          strokeLinecap="round"
+        />
+        <path
+          data-name="Line 70"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 343.566h18.963"
+        />
+        <path
+          data-name="Line 71"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 349.571h31.231"
+        />
+        <path
+          data-name="Line 72"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 355.646h37.926"
+        />
+        <path
+          data-name="Line 73"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 361.721h25.602"
+        />
+        <path
+          data-name="Line 74"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M414.007 367.796h31.231"
+        />
+        <rect
+          data-name="Rectangle 66"
+          width={25.626}
+          height={25.626}
+          rx={2.676}
+          transform="translate(383.256 319.222)"
+          strokeMiterlimit={10}
+        />
+        <path
+          data-name="Line 75"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M482.087 277.142l-18.898 18.898"
+        />
+        <path
+          data-name="Line 76"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M463.189 283.922v12.118"
+        />
+        <path
+          data-name="Line 77"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M475.182 296.04h-11.993"
+        />
+        <path
+          data-name="Line 78"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M349.348 280.532l18.898 18.898"
+        />
+        <path
+          data-name="Line 79"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M356.129 299.431h12.118"
+        />
+        <path
+          data-name="Line 80"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M368.247 287.437v11.993"
+        />
+        <path
+          data-name="Line 81"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M351.892 407.337l18.898-18.898"
+        />
+        <path
+          data-name="Line 82"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M370.791 400.557v-12.118"
+        />
+        <path
+          data-name="Line 83"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M358.797 388.439h11.993"
+        />
+        <path
+          data-name="Line 84"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M482.087 403.946l-18.898-18.898"
+        />
+        <path
+          data-name="Line 85"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M475.307 385.048h-12.118"
+        />
+        <path
+          data-name="Line 86"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          d="M463.189 397.041v-11.993"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default SvgSpecificIcon;
