82ded9f27f48fe2723773ee2459f7418596716f5
benl95 <beno_1995@hotmail.com>
2021-01-20 15:59:18 +0100

    Create Legenda component

---
+++ src/components/Legenda.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './legenda.css';
+
+class Legenda extends React.Component {
+	render() {
+		return (
+			<div className="legendaContainer">
+				<div className="level0"></div>
+				<div className="level1"></div>
+				<div className="level2"></div>
+				<div className="level3"></div>
+				<div className="level4"></div>
+				<div className="level5"></div>
+			</div>
+		);
+	}
+}
+
+export default Legenda;
